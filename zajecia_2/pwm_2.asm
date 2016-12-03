1222d52614625989c28402c14f277d09fd0e6e46
Szymon Kozłowski <szymek.kozlowski1@gmail.com>
2016-12-03 23:38:20 +0100

    Poprawki w komentarzach. Zmiana nazw zmiennych.

--- zajecia_2/pwm_2.asm
+++ zajecia_2/pwm_2.asm
@@ -10,35 +10,29 @@
 
 .org 30h
 INIT:
-	MOV	TMOD,	#01h	; tryb 16-bitowy
-
-	MOV	TL0,	#0BEh	; 0xFCBE = 64702, dla przerwań co 1 ms (1000 Hz)
-	MOV	TH0,	#0FCh
+	MOV	TMOD,	#0x1	; tryb 16-bitowy
+	MOV	TL0,	#0xBE	; 0xFCBE = 64702, dla przerwań co 1 ms (1000 Hz)
+	MOV	TH0,	#0xFC
 
 	CLR	P2.2
+	MOV	P0,	#0xFF
 
-	MOV	R2,	#8	; łączna ilość okresów ON + OFF
-	MOV	R0,	#7	; ilość okresów ON
+	MOV	R1,	#8	; łączna ilość okresów ON + OFF
+	MOV	R0,	#7	; ilość okresów ON w pierwszym przebiegu
 
-	;MOV	R1,	#1
 	CLR	22h.2		; flaga wyboru (1 - ON), (0 - OFF)
 	
-	;MOV	R0,	#7	; inicjalizacja licznika przerwań
 
 	SETB	EA		; globalne zezwolenie na przerwania
 	SETB	ET0		; zezwolenie na przerwania od timera 0
 	SETB	TR0		; uruchomienie T0
 
-;*****TEST********************
-	;CLR P0.0
-	MOV P0, #0FFh
-;*****TEST********************	
 MAIN:
 	JMP	MAIN
 	
 T0_IR:
-	MOV	TL0,	#0BEh	; reset zegara
-	MOV	TH0,	#0FCh
+	MOV	TL0,	#0xBE	; reset zegara
+	MOV	TH0,	#0xFC
 
 	DJNZ	R0,	WAIT_1_MS
 SKIP:
@@ -48,21 +42,19 @@ SKIP:
 	JNB	P0.1,	LED
 	MOV	R0,	#6
 	JNB	P0.2,	LED
-	;MOV	R0,	#8	;R0 się zeruje!!!
-	MOV	R0,	#0	;R0 się zeruje!!!
+	MOV	R0,	#8
+	;MOV	R0,	#0	;R0 się zeruje!!!
 
-;LED:	DJNZ	R1,	ON
-;OFF:	MOV	R1,	#2;
-LED:	JBC	22h.2,	ON
-OFF:	SETB	22h.2
+LED:	JBC	0x22.2,	ON
+OFF:	SETB	0x22.2
 
 	SETB	P2.2		; zgaś diodę
 
-	MOV	A,	R2	; A = ON + OFF
+	MOV	A,	R1	; A = ON + OFF
 	SUBB	A,	R0	; A = A - R0 = OFF
 	MOV	R0,	A	; reset licznika do ilości cykli OFF
 
-	JMP WAIT_1_MS
+	JMP	WAIT_1_MS
 
 ON:	CLR	P2.2		; zapal diodę
 	
