4853bde11b25e18997192d112dd55fed6f311799
Szymon Kozłowski <szymek.kozlowski1@gmail.com>
2016-12-04 00:29:29 +0100

    Dodanie pliku pwm_2_v2 realizującego świecenie diodą z jasnością sterowaną przyciskami.

--- zajecia_2/pwm_2_v2.asm
+++ zajecia_2/pwm_2_v2.asm
@@ -1,4 +1,5 @@
 ;program realizujący PWM przerwania co 1000 Hz
+;przyciski P0.0 - P0.3 sterują jasnością PWM
 
 ;zegar taktowany częstotliwością 10 MHz
 
@@ -14,14 +15,16 @@ INIT:
 	MOV	TL0,	#0xBE	; 0xFCBE = 64702, dla przerwań co 1 ms (1000 Hz)
 	MOV	TH0,	#0xFC
 
-	CLR	P2.2
-	MOV	P0,	#0xFF
-
-	MOV	R1,	#8	; łączna ilość okresów ON + OFF
-	MOV	A,	#7	; ilość okresów ON w pierwszym przebiegu
+	MOV	R2,	#8	; łączna ilość okresów ON + OFF
+	MOV	R1,	#5	; początkowa jasność
 
 	CLR	22h.2		; flaga wyboru (1 - ON), (0 - OFF)
 	
+	MOV	A,	R1
+	MOV	R0,	A	; ilość okresów ON w pierwszym przebiegu
+
+	CLR	P2.2
+	MOV	P0,	#0xFF
 
 	SETB	EA		; globalne zezwolenie na przerwania
 	SETB	ET0		; zezwolenie na przerwania od timera 0
@@ -29,39 +32,57 @@ INIT:
 
 MAIN:
 	JMP	MAIN
-	
+
+
+
 T0_IR:
 	MOV	TL0,	#0xBE	; reset zegara
 	MOV	TH0,	#0xFC
 
 	DJNZ	R0,	WAIT_1_MS
-SKIP:
-	MOV	R0,	#2	; R0 = ON
-	JNB	P0.0,	LED
-	MOV	R0,	#4
-	JNB	P0.1,	LED
-	MOV	R0,	#6
-	JNB	P0.2,	LED
-	MOV	R0,	#8
-	;MOV	R0,	#0	;R0 się zeruje!!!
+	
+CHECK_BTN:
 
-LED:	JBC	0x22.2,	ON
+
+	MOV	A,	P0
+	CJNE	A, #0xFF, CHANGE
+	
+SET_TIME:
+	MOV	A,	R1
+	MOV	R0,	A
+	
+	JBC	0x22.2,	ON	; wybór gałęźi ON/OFF
 OFF:	SETB	0x22.2
 
 	SETB	P2.2		; zgaś diodę
 
-	MOV	A,	R1	; A = ON + OFF
+	MOV	A,	R2	; A = ON + OFF
+	CLR	C
 	SUBB	A,	R0	; A = A - R0 = OFF
 	MOV	R0,	A	; reset licznika do ilości cykli OFF
 
 	JMP	WAIT_1_MS
 
 ON:	CLR	P2.2		; zapal diodę
-	
 
 WAIT_1_MS:
 	MOV	A,	R0
-	JZ	SKIP
+	JZ	CHECK_BTN
 	RETI			; powrót
+
+
+
+
+CHANGE:
+	MOV	R1,	#2	; R0 = ON
+	JNB	P0.0,	SET_TIME
+	MOV	R1,	#4
+	JNB	P0.1,	SET_TIME
+	MOV	R1,	#6
+	JNB	P0.2,	SET_TIME
+	MOV	R1,	#8
+	;MOV	R1,	#0
+	JMP	SET_TIME
 .end
+
 
